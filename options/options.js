2eb0bb594f4c4bc9ae3d88f761d07555425405b0
Crowdbotics <engineering@crowdbotics.com>
2021-08-25 20:58:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "daveotest_4Identifier",
+  url: "https://daveotest_4Identifier.botics.co",
+  api: "https://daveotest_4Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
